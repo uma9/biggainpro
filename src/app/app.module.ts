d49a60c42270526471c866c1a848b5872ea881b9
ravali <ravalichowdary12345@gmail.com>
2019-10-17 17:10:17 +0530

    addpostform

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {MatBadgeModule} from '@angular/material/badge';
 import { RouterModule, Routes } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule ,ReactiveFormsModule } from '@angular/forms';
-// import { MaterialModule } from './material/material.module';
+import { MaterialModule } from './material/material.module';
 import { ScrollingModule } from '@angular/cdk/scrolling';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import { MultiSelectAllModule } from '@syncfusion/ej2-angular-dropdowns';
 import { MultiSelectModule } from '@syncfusion/ej2-angular-dropdowns';
 import { ButtonModule } from '@syncfusion/ej2-angular-buttons';
+import { ComponentMessages } from '@progress/kendo-angular-l10n';
+import { LocalizationService } from '@progress/kendo-angular-l10n';
 import {  
   AgmCoreModule  
 } from '@agm/core'; 
@@ -33,13 +36,15 @@ import { AddpostformComponent } from './addpostform/addpostform.component';
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
     AppRoutingModule,
     BrowserModule,
+    
     BrowserAnimationsModule,  
     FormsModule,
+    
     ReactiveFormsModule,
-    // MaterialModule,
+    MaterialModule,
     ScrollingModule,
     MDBBootstrapModule.forRoot(),
-    NgbModule,
+    NgbModule,MatBadgeModule,
     MultiSelectAllModule,
     MultiSelectModule,
     ButtonModule,
@@ -48,6 +53,7 @@ import { AddpostformComponent } from './addpostform/addpostform.component';
       apiKey: 'Your Google Api Key'  }),
     UploadModule,
     RouterModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
